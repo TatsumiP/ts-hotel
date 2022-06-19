5d40240d5daab361e4147f38f27338e0f84d9062
Tatsumi <tatumistudio@gmail.com>
2022-06-19 17:39:45 +0900

    [add] first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React, {FC, useCallback, useState} from 'react';
+import {Layout} from "./components/layouts";
+import {SearchField} from "./components/organisms";
+import {Hotels} from "./types/hotels";
+
+const App: FC = () => {
+  const [hotels, setHotels] = useState<Hotels>([])
+
+  const handleHotels = useCallback((fetchedHotels: Hotels) => {
+    setHotels([...fetchedHotels])
+  }, [setHotels])
+
+  return (
+    <Layout>
+      <SearchField
+        handleHotels={handleHotels}
+      />
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/lib/hotels.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { APP_ID, BASE_URL } from "rakuten/config";
+import { Hotels } from "types/hotels";
+
+
+// define types of axios data
+interface SearchHotelData {
+    hotels: Hotels
+    pagingInfo: {
+        first: number
+        last: number
+        page: number
+        pageCount: number
+        recordCount: number
+    }
+}
+
+export const searchHotels = async (keyword: string): Promise<Hotels> => {
+    try {
+        const url = BASE_URL + '&applicationId=' + APP_ID + '&keyword=' + keyword
+        const res = await axios.get<SearchHotelData>(url) // axiosはステータスコード200以外の場合、自動で例外をスロー
+        return res.data.hotels
+    } catch (e) {
+        console.error(e)
+        return []
+    }
+}
